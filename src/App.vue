fd2325c37099a0d70480473ad6e483d9ea80ca61
Daniela <dani.velasquezv@gmail.com>
2020-09-11 12:01:02 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <!-- creando input con variable en el estado tarea-->
+    <input class="input" v-model="tarea" type="text" placeholder="Ingresa nueva tarea">
+    <!-- Boton para que agregue tarea el cual tiene un evento click que ejecute método agregar -->
+    <button class="boton" @click="add" >Agregar tarea </button>
+    <tarea v-for="(tarea, i) in tareas" :tarea="tarea" :indice="i" :key="i" @eliminar="eliminarTarea"></tarea>
+  </div>
+</template>
+
+<script>
+import Tarea from './components/Tarea'
+export default {
+  name: "lista",
+  //agregando el input en el estado
+  data() {
+    return {
+      tarea: "",
+      //arreglo vacio
+      tareas: []
+    }
+  },
+  components: {
+    Tarea,
+  },
+  methods: {
+    //creando método agregar, que enviará push a arreglo que será tareas
+    add(){
+      this.tareas.push(this.tarea)
+      this.tarea = ""
+
+    },
+    eliminarTarea(indice){
+      this.tareas.splice(indice, 1)
+    }
+  },
+
+}
+</script>
+
+<style lang="scss">
+  .boton {
+    color: white;
+    background-color: #9729B2;
+    margin-left: 60px;
+    padding: 10px;
+    
+  }
+  .input {
+    padding: 10px;
+  }
+  
+</style>>
---
+++ src/components/Tarea.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <p>{{tarea}}</p>
+      <!--esto tendrá un método del componente tarea-->
+      <button class="boton" @click="vaciar">Eliminar tarea</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+name: "Tarea",
+  //componente recibe props de tarea de tipo string
+  props: {
+    tarea: String,
+    indice: Number,
+  },
+  methods: {
+    //le enviará un evento al papá el que se llamará eliminar
+    vaciar(){
+    //este es un método que recibe 2 parámetros: el primero es el nombre del evento que irá al papá que en este caso será
+    //eliminar y también le pasaremos como argumento indice     
+    this.$emit("eliminar", this.indice)
+      },
+  },
+};
+</script>
+
+<style></style>
